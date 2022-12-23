440ae25db669bad96ed389bd3d56b4ddc7205d9b
MohamedRamadan10 <mr.uiux.dev@gmail.com>
2022-12-23 17:18:10 +0200

    Simple Module

--- 01 node firm/index.js
+++ 01 node firm/index.js
@@ -32,22 +32,7 @@ const http = require("http");
 const url = require("url");
 
 // Top level block code that run once at the begin (Synchronous)
-const replaceTemplate = (temp, product) => {
-	// Regular expression /{%PRODUCTNAME%}/g => global
-	let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-	output = output.replace(/{%IMAGE%}/g, product.image);
-	output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-	output = output.replace(/{%FROM%}/g, product.from);
-	output = output.replace(/{%PRICE%}/g, product.price);
-	output = output.replace(/{%QUANTITY%}/g, product.quantity);
-	output = output.replace(/{%DESCRIPTION%}/g, product.description);
-	output = output.replace(/{%ID%}/g, product.id);
-
-	if (!product.organic)
-		output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-
-	return output;
-};
+const replaceTemplate = require("./modules/replaceTemplate");
 const tempOverview = fs.readFileSync(
 	`${__dirname}/templates/template-overview.html`,
 	"utf-8"
@@ -81,7 +66,7 @@ const server = http.createServer((req, res) => {
 
 		const product = dataObj[query.id];
 		const output = replaceTemplate(tempProduct, product);
-		
+
 		res.end(output);
 	} else if (pathname === "/api") {
 		res.writeHead(200, { "content-type": "application/json" });
