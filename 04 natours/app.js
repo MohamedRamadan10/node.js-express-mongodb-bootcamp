3fc04e5062273e83c956e3fa799e874c8389957f
Mohamed Ramadan <mr.uiux.dev@gmail.com>
2023-01-16 22:48:14 +0200

    Environment - Elsint and Prettier setup

--- 04 natours/app.js
+++ 04 natours/app.js
@@ -1,4 +1,3 @@
-"use strict";
 const express = require("express");
 const morgan = require("morgan");
 const tourRoutes = require("./routes/tourRoutes");
@@ -6,17 +5,20 @@ const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
-app.use(morgan("dev"));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-   console.log("Hello from the middleware 👋");
-   next();
+  console.log("Hello from the middleware 👋");
+  next();
 });
 app.use((req, res, next) => {
-   req.requestTime = new Date().toISOString();
-   next();
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
 app.use("/api/v1/tours", tourRoutes);
--- 04 natours/server.js
+++ 04 natours/server.js
@@ -1,6 +1,8 @@
-"use strict";
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
 
-const port = 5000;
+const port = process.env.Port || 5000;
 app.listen(port, () => console.log(`App is running on port ${port}`));
