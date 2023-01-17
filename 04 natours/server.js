56caf243af8a042fcb979b3ad58d01b2c2f12769
Mohamed Ramadan <mr.uiux.dev@gmail.com>
2023-01-18 00:27:02 +0200

    connect DB with Express

--- 04 natours/server.js
+++ 04 natours/server.js
@@ -1,6 +1,12 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DB.replace("<Password>", process.env.DB_PASSWORD);
+
+mongoose.set("strictQuery", true);
+mongoose.connect(DB).then(() => console.log("DB connection successful!"));
 
 const app = require("./app");
 
