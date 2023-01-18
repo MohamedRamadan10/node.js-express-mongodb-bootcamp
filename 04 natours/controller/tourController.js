5d07a7d69bd656de651982462d08d83fa68083bd
M-Ramadan <mr.uiux.dev@gmail.com>
2023-01-18 17:08:44 +0200

    create document express mongoos

--- 04 natours/controller/tourController.js
+++ 04 natours/controller/tourController.js
@@ -1,66 +1,43 @@
+const Tour = require("../models/tourModel");
+
 /* eslint-disable prefer-object-spread */
-const fs = require("fs");
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id is ${val}`);
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({ status: "Failed", message: "Invalid Id" });
-  }
-  next();
-};
-
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: "Failed",
-      message: "Missing name and price",
-    });
-  }
-  next();
-};
-
 exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    requestedAt: req.requestTime,
-    result: tours.length,
-    data: { tours },
-  });
+  // res.status(200).json({
+  //   status: "success",
+  //   requestedAt: req.requestTime,
+  //   result: tours.length,
+  //   data: { tours },
+  // });
 };
 
 exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-
-  res.status(200).json({
-    status: "success",
-    data: { tour },
-  });
+  // const id = req.params.id * 1;
+  // const tour = tours.find((el) => el.id === id);
+  // res.status(200).json({
+  //   status: "success",
+  //   data: { tour },
+  // });
 };
 
-exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
+exports.createTour = async (req, res) => {
+  // const newTour = new Tour();
+  // newTour.save();
 
-  tours.push(newTour);
+  try {
+    const newTour = await Tour.create(req.body);
 
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: "success",
-        data: {
-          tour: newTour,
-        },
-      });
-      if (err) return console.log(err.message);
-    }
-  );
+    res.status(200).json({
+      message: "success",
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: "Invalid data sent!",
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
--- 04 natours/server.js
+++ 04 natours/server.js
@@ -3,12 +3,11 @@ const dotenv = require("dotenv");
 
 dotenv.config({ path: "./config.env" });
 
-const DB = process.env.DB.replace("<Password>", process.env.DB_PASSWORD);
+const app = require("./app");
 
+const DB = process.env.DB.replace("<Password>", process.env.DB_PASSWORD);
 mongoose.set("strictQuery", true);
 mongoose.connect(DB).then(() => console.log("DB connection successful!"));
-
-const app = require("./app");
 
 const port = process.env.Port || 5000;
 app.listen(port, () => console.log(`App is running on port ${port}`));
