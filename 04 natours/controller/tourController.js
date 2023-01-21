22a27373697917483a4d8a3fdda117fa5665c024
Mohamed Ramadan <mr.uiux.dev@gmail.com>
2023-01-21 15:28:10 +0200

    Sort - Fields Limit - Pagination API

--- 04 natours/controller/tourController.js
+++ 04 natours/controller/tourController.js
@@ -2,25 +2,50 @@ const Tour = require("../models/tourModel");
 
 exports.getAllTours = async (req, res) => {
   try {
-    const queryObj = { ...req.query };
-    const excludedFields = ["page", "sort", "limit", "fields"];
-
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    console.log(req.query, queryObj);
-
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    console.log(queryStr);
-
-    const query = Tour.find(JSON.parse(queryStr));
-
     // const query = await Tour.find()
     //   .where("duration")
     //   .equals(5)
     //   .where("difficulty")
     //   .equals("easy");
+
+    const queryObj = { ...req.query };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // console.log(req.query, queryObj);
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    // console.log(queryStr);
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" "); //sort("price ratingsAverage")
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    // // page=2&limit=10 : 1-10 -> page 1, 11-20 -> page 3, 21-30 -> page 3
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error("This page does not exist");
+    }
 
     const tours = await query;
 
