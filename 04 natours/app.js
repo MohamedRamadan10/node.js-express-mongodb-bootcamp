5427ca813da8fe1f7697eef5e581838124922da8
Mohamed Ramadan <mr.uiux.dev@gmail.com>
2023-02-11 21:48:10 +0200

    Implement a global error handling middleware

--- 04 natours/app.js
+++ 04 natours/app.js
@@ -13,15 +13,34 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log("Hello from the middleware 👋");
-  next();
-});
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
 app.use("/api/v1/tours", tourRoutes);
 app.use("/api/v1/users", userRoutes);
+
+app.all("*", (req, res, next) => {
+  // res.status(404).json({
+  //   status: "Failed",
+  //   message: `Can't find the ${req.originalUrl} on this server!!`,
+  // });
+
+  const err = new Error(`Can't find the ${req.originalUrl} on this server!!`);
+  err.status = "Failed";
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "Error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app;
